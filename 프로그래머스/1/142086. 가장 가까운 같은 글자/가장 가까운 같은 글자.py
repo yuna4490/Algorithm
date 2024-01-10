0955a49a6aeef754183154ad044a6f4c0beebe4e
yunDDi <67116186+yuna4490@users.noreply.github.com>
2024-01-10 22:46:54 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 24.41 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = [-1] * len(s)
+    
+    for i in range(len(s)):
+        for j in range(i-1, -1, -1):
+            if s[i] == s[j]:
+                answer[i] = i-j
+                break
+    
+    return answer
