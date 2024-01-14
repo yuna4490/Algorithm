751c6b4eaca9290405b82811260a2fb948cfe827
yunDDi <67116186+yuna4490@users.noreply.github.com>
2024-01-15 00:12:13 +0900

    [level Lv.1] Title: 콜라 문제, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/132267. 콜라 문제/콜라 문제.py
@@ -0,0 +1,9 @@
+def solution(a, b, n):
+    answer = 0
+    
+    while n >= a:
+        answer += n//a * b
+        n = n - (n // a)*a + (n//a)*b
+        print(n, answer)
+        
+    return answer
