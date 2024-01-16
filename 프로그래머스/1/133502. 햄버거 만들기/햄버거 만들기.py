90ecbf548f11f44d7676ad307d0c4a014173e4e7
yunDDi <67116186+yuna4490@users.noreply.github.com>
2024-01-16 23:10:19 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,30 +1,17 @@
 def solution(ingredient):
     answer = 0
-    stack = []
+    burger = []
     
     # 2 : 야채, 1 : 빵, 3 : 고기
     # 1 2 3 1
     
     for i in ingredient:
-        if not stack:
-            if i == 1:
-                stack.append(i)
+        burger.append(i)
         
-        else:
-            if i == 1:
-                if stack[-1] == 3: 
-                    stack = stack[0:len(stack)-3]
-                    answer += 1
-                
-                else:
-                    stack.append(i)
-        
-            elif stack[-1] == i-1:
-                stack.append(i)
+        if burger[-4:] == [1,2,3,1]:
+            answer += 1
             
-            else:
-                stack = [] 
-    
-        # print(i, stack)
+            for i in range(4):
+                burger.pop()
         
     return answer
